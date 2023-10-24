35b8fa0e1c4daf14b169067ac8465fc57dfdbebe
szczenkowski <mpilkowski.learn@gmail.com>
2023-10-24 14:48:38 +0200

    update repo crash-course
    lessons up to data base mutations

---
+++ graphql-server-example/index.js
@@ -0,0 +1,56 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+// db
+import db from "./_db.js";
+// types
+import { typeDefs } from "./shema.js";
+
+const resolvers = {
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((a) => a.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((g) => g.id === parent.game_id);
+    },
+  },
+  Query: {
+    games() {
+      return db.games;
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    reviews() {
+      return db.reviews;
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+    authors() {
+      return db.authors;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+  },
+};
+
+// server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
+console.log("Server ready at port", 4000);
---
+++ graphql-server-example/shema.js
@@ -0,0 +1,35 @@
+export const typeDefs = `#graphql
+
+type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+    reviews: [Review!]
+}
+type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+    game: Game!
+    author: Author!
+}
+type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+    reviews: [Review!]
+}
+type Query {    
+    reviews: [Review]
+    games: [Game]
+    authors: [Author]
+    review(id: ID!): Review
+    game(id: ID!): Game
+    author(id: ID!): Author
+
+}
+
+`;
+
+// types in graphql : int, float, string, boolean, ID
+// ! means it cannot be null
